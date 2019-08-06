95c49e521883d6bf7792ee24dbe06aabebba20f1
yishibashi <yishibashi101@gmail.com>
2019-08-06 16:44:41 +0900

    day4: ~ 5ポインタの応用

--- day4/bootpack.c
+++ day4/bootpack.c
@@ -5,9 +5,20 @@ extern void _write_mem8(int adr, int data);
 void HariMain(void)
 {
   int i;
+  char *p;
 
   for (i = 0xa0000; i <= 0xaffff; i++) {
-    _write_mem8(i, i & 0x0f); /* MOV BYTE [i],15; */
+
+    p = (char *) i;
+
+    *p = i & 0x0f;
+
+	/* OR
+	 * *(p + i) = i & 0x0f;
+	 * OR
+	 * p[i] = i & 0x0f;
+	 */
+
   }
 
   for (;;) {
--- day4/nasmfunc.asm
+++ day4/nasmfunc.asm
@@ -16,15 +16,8 @@
 
 section .text
 		GLOBAL _io_hlt
-		GLOBAL _write_mem8
 
 _io_hlt:	; void io_hlt(void);
 		HLT
-		RET
-
-_write_mem8:	; void _write_mem8(int addr, int data);
-		MOV			ECX, [ESP+4]			; [ESP+4]にaddrが入っているのでそれをECXに読み込む
-		MOV			AL,[ESP+8]				; [ESP+8]にdataが入っているのでそれをALに読み込む
-		MOV			[ECX],AL
 		RET
 
