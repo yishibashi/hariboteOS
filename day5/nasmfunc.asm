14235b678a6b3f205feea6445f852a43aaaa8138
yishibashi <yishibashi101@gmail.com>
2019-08-12 15:38:41 +0900

    day5

--- day5/nasmfunc.asm
+++ day5/nasmfunc.asm
@@ -19,6 +19,7 @@ section .text
 		GLOBAL _io_in8, _io_in16, _io_in32
 		GLOBAL _io_out8, _io_out16, _io_out32
 		GLOBAL _io_load_eflags, _io_store_eflags
+		GLOBAL _load_gdtr, _load_idtr
 
 
 
@@ -84,4 +85,14 @@ _io_store_eflags:
 		PUSH		EAX
 		POPFD
 		RET
+_load_gdtr:		; void load_gdtr(int limit, int addr)
+		mov		ax, [esp + 4]		; limit
+		mov 	[esp + 6], ax
+		lgdt	[esp + 6]
+		ret
 
+_load_idtr:		; void load_idtr(int limit, int addr)
+		mov		ax, [esp + 4]		; limit
+		mov 	[esp + 6], ax
+		lidt	[esp + 6]
+		ret
