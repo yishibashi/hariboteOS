8f24f875557d888de5efc7fda1bc4de84967afd2
Yoshihiro Ishibashi <yishibashi101@gmail.com>
2019-08-16 16:18:21 +0900

    day6:3

---
+++ day6/bootpack.h
@@ -0,0 +1,135 @@
+/* asmhead.nas */
+struct BOOTINFO { /* 0x0ff0-0x0fff */
+	char cyls; /* ブートセクタはどこまでディスクを読んだのか */
+	char leds; /* ブート時のキーボードのLEDの状態 */
+	char vmode; /* ビデオモード  何ビットカラーか */
+	char reserve;
+	short scrnx, scrny; /* 画面解像度 */
+	char *vram;
+};
+#define ADR_BOOTINFO	0x00000ff0
+
+/* naskfunc.nas */
+extern void io_hlt(void);
+extern void io_cli(void);
+extern void io_out8(int port, int data);
+extern int io_load_eflags(void);
+extern void io_store_eflags(int eflags);
+extern void load_gdtr(int limit, int addr);
+extern void load_idtr(int limit, int addr);
+
+/* graphic.c */
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen8(char *vram, int x, int y);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize);
+
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
+
+/* dsctbl.c */
+struct SEGMENT_DESCRIPTOR {
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+struct GATE_DESCRIPTOR {
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
+};
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+#define ADR_IDT			0x0026f800
+#define LIMIT_IDT		0x000007ff
+#define ADR_GDT			0x00270000
+#define LIMIT_GDT		0x0000ffff
+#define ADR_BOTPAK		0x00280000
+#define LIMIT_BOTPAK	0x0007ffff
+#define AR_DATA32_RW	0x4092
+#define AR_CODE32_ER	0x409a
+
+// sprintf
+#include <stdarg.h>
+int dec2asc (char *str, int dec);
+int hex2asc (char *str, int dec);
+void sprintf (char *str, char *fmt, ...);
+
+//10進数からASCIIコードに変換
+int dec2asc (char *str, int dec) {
+    int len = 0, len_buf; //桁数
+    int buf[10];
+    while (1) { //10で割れた回数（つまり桁数）をlenに、各桁をbufに格納
+        buf[len++] = dec % 10;
+        if (dec < 10) break;
+        dec /= 10;
+    }
+    len_buf = len;
+    while (len) {
+        *(str++) = buf[--len] + 0x30;
+    }
+    return len_buf;
+}
+
+//16進数からASCIIコードに変換
+int hex2asc (char *str, int dec) { //10で割れた回数（つまり桁数）をlenに、各桁をbufに格納
+    int len = 0, len_buf; //桁数
+    int buf[10];
+    while (1) {
+        buf[len++] = dec % 16;
+        if (dec < 16) break;
+        dec /= 16;
+    }
+    len_buf = len;
+    while (len) {
+        len --;
+        *(str++) = (buf[len]<10)?(buf[len] + 0x30):(buf[len] - 9 + 0x60);
+    }
+    return len_buf;
+}
+
+void sprintf (char *str, char *fmt, ...) {
+    va_list list;
+    int i, len;
+    va_start (list, 2);
+
+    while (*fmt) {
+        if(*fmt=='%') {
+            fmt++;
+            switch(*fmt){
+                case 'd':
+                    len = dec2asc(str, va_arg (list, int));
+                    break;
+                case 'x':
+                    len = hex2asc(str, va_arg (list, int));
+                    break;
+            }
+            str += len; fmt++;
+        } else {
+            *(str++) = *(fmt++);
+        }	
+    }
+    *str = 0x00; //最後にNULLを追加
+    va_end (list);
+}
+
