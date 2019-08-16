ed6c7bb814555330650ba4b3de1b55dc795f174a
Yoshihiro Ishibashi <yishibashi101@gmail.com>
2019-08-16 16:45:05 +0900

    day6:5

--- day6/bootpack.c
+++ day6/bootpack.c
@@ -2,28 +2,28 @@
 
 void HariMain(void)
 {
-	char *vram;
-	int xsize, ysize;
-	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+
+	init_gdtidt();
+	init_pic();
 
 	init_palette();
-
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
 
 
 	char s[40];
-	sprintf(s, "scrnx = %d", binfo->scrnx);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 16,64, COL8_FFFFFF, s);
 
 
 	int mx, my;
 	char mcursor[256];
+
 	mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
 	my = (binfo->scrny - 28 - 16) / 2;
 	init_mouse_cursor8(mcursor, COL8_008484);
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
 
 
   for (;;) {
--- day6/bootpack.h
+++ day6/bootpack.h
@@ -69,67 +69,18 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 #define AR_DATA32_RW	0x4092
 #define AR_CODE32_ER	0x409a
 
-// sprintf
-#include <stdarg.h>
-int dec2asc (char *str, int dec);
-int hex2asc (char *str, int dec);
-void sprintf (char *str, char *fmt, ...);
-
-//10進数からASCIIコードに変換
-int dec2asc (char *str, int dec) {
-    int len = 0, len_buf; //桁数
-    int buf[10];
-    while (1) { //10で割れた回数（つまり桁数）をlenに、各桁をbufに格納
-        buf[len++] = dec % 10;
-        if (dec < 10) break;
-        dec /= 10;
-    }
-    len_buf = len;
-    while (len) {
-        *(str++) = buf[--len] + 0x30;
-    }
-    return len_buf;
-}
-
-//16進数からASCIIコードに変換
-int hex2asc (char *str, int dec) { //10で割れた回数（つまり桁数）をlenに、各桁をbufに格納
-    int len = 0, len_buf; //桁数
-    int buf[10];
-    while (1) {
-        buf[len++] = dec % 16;
-        if (dec < 16) break;
-        dec /= 16;
-    }
-    len_buf = len;
-    while (len) {
-        len --;
-        *(str++) = (buf[len]<10)?(buf[len] + 0x30):(buf[len] - 9 + 0x60);
-    }
-    return len_buf;
-}
-
-void sprintf (char *str, char *fmt, ...) {
-    va_list list;
-    int i, len;
-    va_start (list, 2);
-
-    while (*fmt) {
-        if(*fmt=='%') {
-            fmt++;
-            switch(*fmt){
-                case 'd':
-                    len = dec2asc(str, va_arg (list, int));
-                    break;
-                case 'x':
-                    len = hex2asc(str, va_arg (list, int));
-                    break;
-            }
-            str += len; fmt++;
-        } else {
-            *(str++) = *(fmt++);
-        }	
-    }
-    *str = 0x00; //最後にNULLを追加
-    va_end (list);
-}
+/* int.c */
+void init_pic(void);
+#define PIC0_ICW1		0x0020
+#define PIC0_OCW2		0x0020
+#define PIC0_IMR		0x0021
+#define PIC0_ICW2		0x0021
+#define PIC0_ICW3		0x0021
+#define PIC0_ICW4		0x0021
+#define PIC1_ICW1		0x00a0
+#define PIC1_OCW2		0x00a0
+#define PIC1_IMR		0x00a1
+#define PIC1_ICW2		0x00a1
+#define PIC1_ICW3		0x00a1
+#define PIC1_ICW4		0x00a1
 
