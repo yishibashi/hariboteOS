45fa03df436f7f07faae0b56a60a7de776c8764b
Yoshihiro Ishibashi <yishibashi101@gmail.com>
2019-08-19 15:13:41 +0900

    day7:6

--- day7/bootpack.c
+++ day7/bootpack.c
@@ -1,6 +1,8 @@
 #include "bootpack.h"
 
 extern struct FIFO8 keyfifo;
+void enable_mouse(void);
+void init_keyboard(void);
 
 void HariMain(void)
 {
@@ -11,6 +13,11 @@ void HariMain(void)
 	init_gdtidt();
 	init_pic();
 	io_sti(); // IDT/PICの初期化が終わったのでCPUの割り込み禁止を解除
+	fifo8_init(&keyfifo, 32, keybuf);
+	io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
+	io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
+
+	init_keyboard();
 
 	init_palette();
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -21,10 +28,7 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	fifo8_init(&keyfifo, 32, keybuf);
-	io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
-	io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
-	
+
 	for (;;) {
 
 		io_cli();
@@ -38,4 +42,45 @@ void HariMain(void)
 			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
 		}
 	}
+}
+
+#define PORT_KEYDAT				0x0060
+#define PORT_KEYSTA				0x0064
+#define PORT_KEYCMD				0x0064
+#define KEYSTA_SEND_NOTREADY	0x02
+#define KEYCMD_WRITE_MODE		0x60
+#define KBC_MODE				0x47
+
+void wait_KBC_sendready(void)
+{
+	/* キーボードコントローラがデータ送信可能になるのを待つ */
+	for (;;) {
+		if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
+			break;
+		}
+	}
+	return;
+}
+
+void init_keyboard(void)
+{
+	/* キーボードコントローラの初期化 */
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, KBC_MODE);
+	return;
+}
+
+#define KEYCMD_SENDTO_MOUSE		0xd4
+#define MOUSECMD_ENABLE			0xf4
+
+void enable_mouse(void)
+{
+	/* マウス有効 */
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+	return; /* うまくいくとACK(0xfa)が送信されてくる */
 }
